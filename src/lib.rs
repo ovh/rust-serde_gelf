997e1c376c3fb7deda5680ffa6cc471d7d6e611b
Cedric Dumay <cedric.dumay@gmail.com>
2018-12-21 03:48:23 +0100

    cleaning code

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,6 @@ extern crate serde;
 extern crate serde_value;
 extern crate serde_json;
 
-#[macro_use]
-extern crate serde_derive;
-
 mod ser;
 
 #[derive(Debug)]
@@ -35,40 +32,3 @@ pub fn to_string<T: ?Sized>(value: &T) -> Result<String> where T: serde::Seriali
     let f = ser::GelfField::new("", "", &serde_value::to_value(value)?);
     Ok(serde_json::to_string(&f.disassemble())?)
 }
-
-//
-//fn main() {
-//    use serde_value::Value;
-//    use std::collections::HashMap;
-//
-//    #[derive(Serialize)]
-//    struct SubFoo {
-//        sa: String,
-//        sb: isize,
-//    }
-//
-//    #[derive(Serialize)]
-//    struct Foo {
-//        a: u32,
-//        b: String,
-//        c: Vec<bool>,
-//        d: HashMap<String, Value>,
-//        e: SubFoo,
-//    }
-//
-//    let foo = Foo {
-//        a: 15,
-//        b: "hello".into(),
-//        c: vec![true, false],
-//        d: {
-//            let mut map = HashMap::new();
-//            map.insert("k1".to_string(), Value::F64(5.9));
-//            map.insert("k2".to_string(), Value::Bool(false));
-//            map
-//        },
-//        e: SubFoo { sa: "test".to_string(), sb: 5 },
-//    };
-//    let _ = to_string_pretty(&foo);
-////    println!("{}", to_string_pretty(&foo).unwrap());
-////    println!("{}", to_string(&foo).unwrap());
-//}
